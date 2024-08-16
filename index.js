4fa0870e79eecd671e80839da6a2a60ccd2be4d9
João Victor <144742430+jvictormo@users.noreply.github.com>
2024-08-16 10:21:28 -0300

    Adding lib

---
+++ index.js
@@ -0,0 +1,5 @@
+mensagem = () => {
+    console.log(`Olha vc aqui Lucas Kwnsi: https://pbs.twimg.com/ext_tw_video_thumb/1546943149322473472/pu/img/V7a-yDzv1fi4mxqB.jpg`)
+}
+
+export default mensagem
