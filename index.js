bf9a46b1136de8c2ca5df869af0aee6d789654a4
João Victor <144742430+jvictormo@users.noreply.github.com>
2024-08-16 10:26:47 -0300

    Fixing issues in index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-mensagem = () => {
+let mensagem = () => {
     console.log(`Olha vc aqui Lucas Kwnsi: https://pbs.twimg.com/ext_tw_video_thumb/1546943149322473472/pu/img/V7a-yDzv1fi4mxqB.jpg`)
 }
 
